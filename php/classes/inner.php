b6ce25b258e4f1787194e2f63f6efb4f5b0b33b8
KIMB-technologies <info@kimb-technologies.eu>
2019-09-12 21:51:30 +0200

    fixed #3

--- php/classes/inner.php
+++ php/classes/inner.php
@@ -1,106 +1,154 @@
 <?php
 defined('HAMA-Radio') or die('Invalid Endpoint');
 
-function filterURL(string $url) : string {
-	$url = filter_var( $url, FILTER_VALIDATE_URL) ? substr( $url , 0, 1000) : 'invalid';
-	return empty($url) ? '' : $url;
-}
-function filterName(string $name): string{
-	$name = str_replace( ['ä','ü','ß','ö','Ä','Ü','Ö'], ['ae','ue','ss','oe','Ae','Ue','Oe'], $name);
-	$name = substr( preg_replace( '/[^0-9A-Za-z \.\-\_\,\&\;\/\(\)]/', '',  $name ), 0, 200 );
-	return empty($name) ? 'empty' : $name;
-}
+class Inner {
 
-if(isset( $_GET['radios'] ) && isset( $_POST['name'] )){
-	echo '<span style="color:green;">Radiosender geändert!</span>';
+	private $html = array();
+	private $radios, $podcasts;
 
-	$radios = array();
-	foreach( $_POST['name'] as $id => $name ){
-		if( !empty($name) ){
-			$radios[] = array(
-				'name' => filterName( $name ),
-				'url' => filterURL( $_POST['url'][$id] ),
-				'logo' => filterURL( $_POST['logo'][$id] ),
-				'desc' => filterName( $_POST['desc'][$id] )
-			);
+	public function __construct(){
+		$this->radios = json_decode( file_get_contents( __DIR__ . '/../data/radios.json' ), true);
+		$this->podcasts = json_decode( file_get_contents( __DIR__ . '/../data/podcasts.json' ), true);
+	}
+
+	public function checkPost() {
+		if(isset( $_GET['radios'] ) && isset( $_POST['name'] )){
+			$this->html[] = '<span style="color:green;">Radiosender geändert!</span>';
+		
+			$this->radios = array();
+			foreach( $_POST['name'] as $id => $name ){
+				if( !empty($name) ){
+					$this->radios[] = array(
+						'name' => self::filterName( $name ),
+						'url' => self::filterURL( $_POST['url'][$id] ),
+						'logo' => self::filterURL( $_POST['logo'][$id] ),
+						'desc' => self::filterName( $_POST['desc'][$id] )
+					);
+				}
+			}
+
+			
+		}
+		else if(isset( $_GET['podcasts'] ) && isset( $_POST['name'] ) ){
+			$this->html[] = '<span style="color:green;">Podcasts geändert!</span>';
+		
+			$this->podcasts = array();
+			foreach( $_POST['name'] as $id => $name ){
+				if( !empty($name) ){
+					$this->podcasts[] = array(
+						'name' => self::filterName( $name ),
+						'url' => self::filterURL( $_POST['url'][$id] ),
+						'finalurl' => isset($_POST['finalurl'][$id]) && $_POST['finalurl'][$id] == 'yes'
+					);
+				}
+			}
+			
 		}
 	}
-	file_put_contents( __DIR__ . '/../data/radios.json', json_encode($radios, JSON_PRETTY_PRINT));
-}
-else if(isset( $_GET['podcasts'] ) && isset( $_POST['name'] ) ){
-	echo '<span style="color:green;">Podcasts geändert!</span>';
 
-	$podcasts = array();
-	foreach( $_POST['name'] as $id => $name ){
-		if( !empty($name) ){
-			$podcasts[] = array(
-				'name' => filterName( $name ),
-				'url' => filterURL( $_POST['url'][$id] ),
-				'finalurl' => isset($_POST['finalurl'][$id]) && $_POST['finalurl'][$id] == 'yes'
+	public function addForm(){
+		$this->html[] = '<h3>Radiosender</h3>';
+		$this->html[] = '<form action="?radios" method="post">';
+		$this->html[] = '<table>';
+		$this->html[] = $this->radioForm();
+		$this->html[] = '</table>';
+		$this->html[] = '<input type="submit" value="Sichern">';
+		$this->html[] = '</form>';
+
+		$this->html[] = '<h3>Podcasts</h3>';
+		$this->html[] = '<form action="?podcasts" method="post">';
+		$this->html[] = '<table>';
+		$this->html[] = $this->podcastForm();
+		$this->html[] = '</table>';
+		$this->html[] = '<input type="submit" value="Sichern">';
+		$this->html[] = '</form>';
+	}
+
+	private function radioForm(){
+		$head .= '<tr><th>ID</th><td></td><td style="width: 500px; max-width:60%; "></td></tr>';
+		$rows = array();
+		$count = 0;
+		foreach($this->radios as $key => $radio ){
+			$id = ($key+1000);
+
+			$rows[] = array(  '<b>'. $id .'</b>', '', '' );
+			$rows[] = array(  '', 'Name', '<input delid="d'.$id.'" type="text" value="'.$radio['name'].'" name="name['.$count.']"/>' );
+			$rows[] = array(  '', 'URL', '<input delid="d'.$id.'" type="text" value="'.$radio['url'].'" name="url['.$count.']"/>' );
+			$rows[] = array(  '', 'Logo', '<input delid="d'.$id.'" type="text" value="'.$radio['logo'].'" name="logo['.$count.']"/>' );
+			$rows[] = array(  '', 'Beschreibung',
+				'<input delid="d'.$id.'" type="text" value="'.$radio['desc'].'" name="desc['.$count.']"/>' .
+				'<button class="del" delid="d'.$id.'" type="button" title="Löschen.">&cross;</button>'
 			);
+			$count++;
 		}
+		$rows[] = array(  '<b>Neu</b>', '', '' );
+		$rows[] = array(  '', 'Name', '<input type="text" placeholder="Name" name="name['.$count.']"/>' );
+		$rows[] = array(  '', 'URL', '<input type="text" placeholder="URL (MP3, ...)" name="url['.$count.']"/>' );
+		$rows[] = array(  '', 'Logo', '<input type="text" placeholder="Logo (PNG, ...)" name="logo['.$count.']"/>' );
+		$rows[] = array(  '', 'Beschreibung', '<input type="text" placeholder="Beschreibung" name="desc['.$count.']"/>' );
+		return $head . PHP_EOL . implode( PHP_EOL , array_map( function ($c) {
+			return '<tr><td>' . implode( '</td><td>', $c ) . '</td></tr>';
+		} , $rows ) );
 	}
-	file_put_contents( __DIR__ . '/../data/podcasts.json', json_encode($podcasts, JSON_PRETTY_PRINT));
-}
 
-//load in unloaded
-if( !isset($radios) ){
-	$radios = json_decode( file_get_contents( __DIR__ . '/../data/radios.json' ), true);
-}
-if(!isset( $podcasts )){
-	$podcasts = json_decode( file_get_contents( __DIR__ . '/../data/podcasts.json' ), true);
+	private function podcastForm(){
+		$head = '<tr><th>ID</th><td></td><td style="width: 500px; max-width:60%;"></td></tr>';
+		$rows = array();
+		$count = 0;
+		foreach($this->podcasts as $key => $pod ){
+			$id = ($key+3000);
+
+			$rows[] = array(  '<b>'. $id .'</b>', '', '' );
+			$rows[] = array(  '', 'Name', '<input type="text" delid="d'.$id.'" value="'.$pod['name'].'" name="name['.$count.']"/>' );
+			$rows[] = array(  '', 'URL', '<input type="text" delid="d'.$id.'" value="'.$pod['url'].'" name="url['.$count.']"/>' );
+			$rows[] = array(  '', 'EndURL',
+				'<input type="radio" delid="d'.$id.'" value="yes" name="finalurl['.$count.']" '. ( $pod['finalurl'] ? 'checked="checked"' : '' ) .' /> &check;' .
+				'<input type="radio" delid="d'.$id.'" value="no" name="finalurl['.$count.']" '. ( !$pod['finalurl'] ? 'checked="checked"' : '' ) .' /> &cross;',
+				'<button class="del" delid="d'.$id.'" type="button" title="Löschen.">&cross;</button>'
+			);
+			$count++;
+		}
+		$rows[] = array('<b>Neu</b>', '', '');
+		$rows[] = array('', 'Name', '<input type="text" placeholder="Name" name="name['.$count.']"/>');
+		$rows[] = array('', 'URL', '<input type="text" placeholder="URL" name="url['.$count.']"/>');
+		$rows[] = array(  '', 'EndURL',
+				'<input type="radio" delid="d'.$id.'" value="yes" name="finalurl['.$count.']" /> &check;' .
+				'<input type="radio" delid="d'.$id.'" value="no" name="finalurl['.$count.']" checked="checked" /> &cross;'
+		);
+		return $head . PHP_EOL . implode( PHP_EOL , array_map( function ($c) {
+			return '<tr><td>' . implode( '</td><td>', $c ) . '</td></tr>';
+		} , $rows ) );
+	}
+
+	public function getHTML() : string {
+		$this->html[] = '<script>
+		$(function (){
+			$("button.del").click( function (){
+				var id = $(this).attr("delid");
+				$("input[delid="+ id +"]").val("");
+			});
+		});
+		</script>';
+
+		return implode(PHP_EOL, $this->html);
+	}
+
+	public function __destruct(){
+		file_put_contents( __DIR__ . '/../data/radios.json', json_encode($this->radios, JSON_PRETTY_PRINT));
+		file_put_contents( __DIR__ . '/../data/podcasts.json', json_encode($this->podcasts, JSON_PRETTY_PRINT));
+	}
+
+	// ==== //
+
+	public static function filterURL(string $url) : string {
+		$url = filter_var( $url, FILTER_VALIDATE_URL) ? substr( $url , 0, 1000) : 'invalid';
+		return empty($url) ? '' : $url;
+	}
+
+	public static function filterName(string $name): string{
+		$name = str_replace( ['ä','ü','ß','ö','Ä','Ü','Ö'], ['ae','ue','ss','oe','Ae','Ue','Oe'], $name);
+		$name = substr( preg_replace( '/[^0-9A-Za-z \.\-\_\,\&\;\/\(\)]/', '',  $name ), 0, 200 );
+		return empty($name) ? 'empty' : $name;
+	}
 }
 ?>
-
-<h3>Radiosender</h3>
-<form action="?radios" method="post">
-<table>
-	<tr><th>ID</th><td></td><td style="width: 500px; max-width:60%; "></td></tr>
-<?php
-	foreach($radios as $key => $radio ){
-		$id = ($key+1000);
-		echo '<tr><th>'. $id .'</th><td></td><td></td></tr>';
-		echo '<tr><td></td><td>Name</td><td><input delid="d'.$id.'" type="text" value="'.$radio['name'].'" name="name[]"/></td></tr>';
-		echo '<tr><td></td><td>URL</td><td><input delid="d'.$id.'" type="text" value="'.$radio['url'].'" name="url[]"/></td></tr>';
-		echo '<tr><td></td><td>Logo</td><td><input delid="d'.$id.'" type="text" value="'.$radio['logo'].'" name="logo[]"/></td></tr>';
-		echo '<tr><td></td><td>Beschreibung</td><td><input delid="d'.$id.'" type="text" value="'.$radio['desc'].'" name="desc[]"/></td><td><button class="del" delid="d'.$id.'" type="button" title="Löschen.">&cross;</button></td></tr>';
-	}
-?>
-	<tr><th>New</th><td></td><td></td></tr>
-	<tr><td></td><td>Name</td><td><input type="text" placeholder="Name" name="name[]"/></td></tr>
-	<tr><td></td><td>URL</td><td><input type="text" placeholder="URL (MP3, ...)" name="url[]"/></td></tr>
-	<tr><td></td><td>Logo</td><td><input type="text" placeholder="Logo (PNG, ...)" name="logo[]"/></td></tr>
-	<tr><td></td><td>Beschreibung</td><td><input type="text" placeholder="Beschreibung" name="desc[]"/></td></tr>
-</table>
-<input type="submit" value="Sichern">
-</form>
-
-<h3>Podcasts</h3>
-<form action="?podcasts" method="post">
-<table>
-	<tr><th>ID</th><td></td><td style="width: 500px; max-width:60%; "></td></tr>
-<?php
-	foreach($podcasts as $key => $pod ){
-		$id = ($key+3000);
-		echo '<tr><th>'. $id .'</th><td></td><td></td></tr>';
-		echo '<tr><td></td><td>Name</td><td><input type="text" delid="d'.$id.'" value="'.$pod['name'].'" name="name[]"/></td></tr>';
-		echo '<tr><td></td><td>URL</td><td><input type="text" delid="d'.$id.'" value="'.$pod['url'].'" name="url[]"/></td><td></tr>';
-		echo '<tr><td></td><td>FinalURL</td><td><input type="checkbox" delid="d'.$id.'" value="yes" name="finalurl[]" '. ( $pod['finalurl'] ? 'checked="checked"' : '' ) .' />Follow all redirects on audio urls</td><td><button class="del" delid="d'.$id.'" type="button" title="Löschen.">&cross;</button></td></tr>';
-	}
-?>
-	<tr><th>New</th><td></td><td></td></tr>
-	<tr><td></td><td>Name</td><td><input type="text" placeholder="Name" name="name[]"/></td></tr>
-	<tr><td></td><td>URL</td><td><input type="text" placeholder="URL (RSS Atom, Nextcloud Share [no SSL, no password] ...)" name="url[]"/></td></tr>
-	<tr><td></td><td>FinalURL</td><td><input type="checkbox" name="finalurl[]" value="yes"/> Follow all redirects on audio urls</td></tr>
-</table>
-<input type="submit" value="Sichern">
-</form>
-
-<script>
-$(function (){
-	$("button.del").click( function (){
-		var id = $(this).attr("delid");
-		$("input[delid="+ id +"]").val('');
-	});
-});
-</script>
--- php/gui/index.php
+++ php/gui/index.php
@@ -3,7 +3,11 @@ define('HAMA-Radio', 'Radio');
 error_reporting(0);
 
 require_once( __DIR__ . '/../classes/Config.php' );
+require_once(  __DIR__ . '/../classes/Inner.php');
 Config::checkAccess();
+
+$inner = new Inner();
+$inner->checkPost();
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -37,6 +41,18 @@ Config::checkAccess();
 		div#apiviewer a{
 			text-decoration: none;
 		}
+		div.achtung{
+			border: 2px solid red;
+			padding: 5px;
+			border-radius: 4px;
+			background-color: orange;
+		}
+		div.note{
+			border: 2px solid red;
+			padding: 5px;
+			border-radius: 4px;
+			background-color: lightblue;
+		}
 		</style>
 		<title>Radio API &ndash; Backend</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -45,24 +61,57 @@ Config::checkAccess();
 		<script src="viewer.js"></script>
 	</head>
 	<body>
-		<h1>
-			Radio API &ndash; Backend
-		</h1>
+		<h1>Radio API &ndash; Backend</h1>
 
-		<h2>Podcasts &amp; Radiosender</h2>
-		
-		<?php
-			require_once(  __DIR__ . '/../classes/inner.php');
+			<h2>Podcasts &amp; Radiosender</h2>
 
-			echo '<script> var serverurl = "http'. ( empty($_SERVER['HTTPS']) ? '' : 's' ) .':'. substr(Config::DOMAIN, strpos(Config::DOMAIN, '//')) .'"; </script>';
-		?>
+			<div class="note">
+				Der Name eines Radiosenders wird in der GUI des Radios angezeigt,
+				als URL wird ein streamfähiger Link benötigt (MP3 etc.).
+				Über Logo kann eine URL zu einem den Sender illustrieredenen 
+				Bild angegeben werden. Wird kein Bild angegeben, so wird
+				<a href="<?php echo Config::DOMAIN; ?>media/default.png" target="_blank">
+				<code><?php echo Config::DOMAIN; ?>media/default.png</code></a>
+				im Radio angezeigt. Beschreibung kann leer bleiben und ist 
+				eine weitere Beschreibung des Sender.
+			</div>
+			<p></p>
 
-		<h2>Vorschau</h2>
+			<?php
+				$inner->addForm();
+				echo $inner->getHTML();
+			?>
 
-		<div id="apiviewer">
-		</div>
-		<div id="audiodiv">
-		</div>
+				<p></p>
+				<div class="note">
+					Der Name eines Podcasts wird in der GUI des Radios angezeigt,
+					als URL kann entweder ein RSS Atom Feed angegeben werden oder 
+					der Link zu einer Nextcloud Freigabe. (Letzterer muss mit 
+					<code><?php echo Config::NEXTCLOUD; ?></code> beginnen
+					und die Form <code><?php echo Config::NEXTCLOUD; ?>s/sdHvTUVtVHb/</code>
+					haben. Die Freigabe darf kein Passwort haben.)<br/>
+					Einige Anbieter wie Soundcloud leiten in den Links zu den 
+					Episoden auf SSL um, die eigentliche URL zur Audiodatei ist jedoch 
+					auch ohne SSL erreichbar. Mit EndURL wird der direkte Link zur
+					Audiodatei an das Radio geschickt.
+				</div>
+
+			<h2>Vorschau</h2>
+
+				<div id="apiviewer">
+				</div>
+				<div id="audiodiv">
+				</div>
+
+				<div class="achtung">
+					Achtung, die Vorschau dient in erster Linie dazu die Liste der Radios, Podcast, Episoden 
+					und Streams anzuschauen. Das Abspielen von einigen Audioformaten klappt in 
+					Browsern nicht, dafür aber auf dem Radio. Weiterhin kann das Radio kein SSL 
+					ein Browser jedoch schon.
+				</div>
+<?php
+				echo '<script> var serverurl = "http'. ( empty($_SERVER['HTTPS']) ? '' : 's' ) .':'. substr(Config::DOMAIN, strpos(Config::DOMAIN, '//')) .'"; </script>';
+?>
 
 		<h1></h1>
 
