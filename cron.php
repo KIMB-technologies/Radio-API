90beeb082d7f71d2604c23703223ba0c2afbb9e4
KIMB-technologies <info@kimb-technologies.eu>
2022-01-28 20:41:51 +0100

    Chnage year in copyright

--- cron.php
+++ cron.php
@@ -4,7 +4,7 @@
  * Radio-API
  * https://github.com/KIMB-technologies/Radio-API
  * 
- * (c) 2019 - 2020 KIMB-technologies 
+ * (c) 2019 - 2022 KIMB-technologies 
  * https://github.com/KIMB-technologies/
  * 
  * released under the terms of GNU Public License Version 3
